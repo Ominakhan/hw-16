79f6b2d1b4e50dc803db6a6c91d79314608365de
Omina <omina.mamatalieva.ch@gmail.com>
2023-03-04 18:21:34 +0600

    hw-16

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { Route,Routes,HashRouter } from 'react-router-dom';
+import './App.css';
+import MyCart from './components/MyCart';
+import MyOrders from './components/MyOrders';
+import ProductName1 from './components/ProductName1';
+import ProductName2 from './components/ProductName2';
+import HeaderProducts from './components/HeaderProducts';
+import MainProducts from './components/MainProducts';
+
+
+function AppRout() {
+  return (
+    <div className="App">
+      <HeaderProducts/>
+   <main>
+    <Routes>
+        <Route path="/mainproducts" element={<MainProducts />} />
+        <Route path="/mainproducts/:productname1" element={<ProductName1 />} />
+        <Route path="/mainproducts/:productname2" element={<ProductName2 />} />
+        <Route path="/mycart" element={<MyCart/>} />
+        <Route path="/myorders" element={<MyOrders/>} />
+    </Routes>
+   </main>
+   </div>
+  );
+}
+ const App = () => {
+  return (
+    <HashRouter>
+      <AppRout/>
+    </HashRouter>
+  )
+ }
+
+export default App;
